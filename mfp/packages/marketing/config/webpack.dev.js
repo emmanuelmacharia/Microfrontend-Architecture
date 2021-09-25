0b44fdb5d3c5abf577efee850885b518c62fe647
emmanuel macharia <samuelmarsha@outlook.com>
2021-09-25 13:51:18 +0300

    added production configs and build scripts for the container and the marketing frontends

--- mfp/packages/marketing/config/webpack.dev.js
+++ mfp/packages/marketing/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 const commonConfig = require('./webpack.common.js');
@@ -15,16 +14,12 @@ const devConfig = {
     },
 
     plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        }),
         new ModuleFederationPlugin({
             name: 'marketing',
             filename: 'remoteEntry.js',
             exposes: {
                 './MarketingApp': './src/bootstrap'
             },
-            // shared: ['react', 'react-dom']
             shared: packageJson.dependencies
         })
     ]
